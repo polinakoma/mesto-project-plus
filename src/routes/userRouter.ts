3e253a5d254d18caa9de21c4547ed4ca5c7e0937
Polina Komarova <polina-komarova-95@mail.ru>
2023-04-30 14:11:34 +0300

    bug fix 1

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -5,10 +5,10 @@ import {
 
 const userRouter = Router();
 
-userRouter.get('/users', getAllUsers);
-userRouter.get('/users/:userId', getUserById);
-userRouter.post('/users', createUser);
-userRouter.patch('/users/me', editProfile);
-userRouter.patch('/users/me/avatar', editAvatar);
+userRouter.get('/', getAllUsers);
+userRouter.post('/', createUser);
+userRouter.get('/:userId', getUserById);
+userRouter.patch('/me', editProfile);
+userRouter.patch('/me/avatar', editAvatar);
 
 export default userRouter;
